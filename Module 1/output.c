f9edf8bde77d76f7a4da7dc67e5769530084e2ca
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-02 00:47:10 +0600

    output.c

---
+++ Module 1/output.c
@@ -0,0 +1,34 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     char c;
+//     float f;
+//     scanf("%d %c %f", &a,&c,&f);
+//     printf("%d %c %.2f\n\n\n",a,c,f);
+
+    
+//     return 0;
+// }
+
+#include<stdio.h>
+int main()
+{
+    // print 10% 20%
+
+    // int a,b;
+    // char p;
+
+    // scanf("%d%c %d%c",&a,&p,&b,&p);
+    // printf("%d%% %d%%",a,b);
+
+    int a,b;
+
+     scanf("%d%% %d%%",&a,&b);
+     printf("%d%% %d%%",a,b);
+
+     printf("\n\n");
+     scanf("%da %db",&a,&b);
+     printf("%da %db",a,b);
+    return 0;
+}
