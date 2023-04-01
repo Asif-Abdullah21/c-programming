fb1202f7d51eb1d361559942eaf8ff2748687c90
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-02 00:17:54 +0600

    print.c

---
+++ Module 1/print.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    printf("\\\\");
+    printf("%%");
+    printf("Hello\tworld\nNow I am programmer,yee\n\n");
+  //  printf("Now I am programmer");
+    int n=10;
+    float x = 10000.52;
+    char ch = 'A';
+    printf("%d %0.2f %c",n,x,ch); 
+    return 0;
+}
+
+//\t - 4 ta space
+// / r % print krte chaile 2 ta kre dite hoy
+// hello world
+// now i am a programmer
