6c7e66f5043bd0458affdc141a8c978290f709cf
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-02 01:01:53 +0600

    output.c

--- Module 1/output.c
+++ Module 1/output.c
@@ -22,13 +22,27 @@ int main()
     // scanf("%d%c %d%c",&a,&p,&b,&p);
     // printf("%d%% %d%%",a,b);
 
-    int a,b;
+    // int a,b;
 
-     scanf("%d%% %d%%",&a,&b);
-     printf("%d%% %d%%",a,b);
+    //  scanf("%d%% %d%%",&a,&b);
+    //  printf("%d%% %d%%",a,b);
 
-     printf("\n\n");
-     scanf("%da %db",&a,&b);
-     printf("%da %db",a,b);
+    //  printf("\n\n");
+    //  scanf("%da %db",&a,&b);
+    //  printf("%da %db",a,b);
+
+    //  printf("\n\n");
+    int a=1000000000;
+    long long int aa=1000000000000000000;
+    printf("%d\n",a);
+    printf("%lld\n",aa);
+     float ff= 22.123456789;
+     double dd=2.123456789123456;
+      double ddd=22.123456789123456;
+
+     printf("%f\n",ff);
+     printf("%.15lf\n",dd);
+    printf("%.15lf",ddd);
+
     return 0;
 }
---
+++ Module 1/tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+int a,b;
+
+    //  scanf("%d%% %d%%",&a,&b);
+    //  printf("%d%% %d%%",a,b);
+
+    //  printf("\n\n");
+    //  scanf("%da %db",&a,&b);
+    //  printf("%da %db",a,b);
+
+    //  printf("\n\n");
