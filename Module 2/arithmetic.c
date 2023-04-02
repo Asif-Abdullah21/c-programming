afc009d93bf65dc7d50b6f5035dc0ad8f25d26a8
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-02 22:18:14 +0600

    arithmetic operator testing

---
+++ Module 2/arithmetic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a=3,b=20;
+    double divide = b*1.0/a;
+    printf("%lf\n",divide);
+
+    int mod = b%a;
+    int mod1 = a%b;
+    printf("%d\n",mod);
+    printf("%d\n",mod1);  
+    return 0;
+}
