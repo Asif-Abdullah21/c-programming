30d872b7909a97c8bd4eaaf83273ea56cfb0c8ef
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-02 23:06:21 +0600

    conditional statement-if else

---
+++ Module 2/conditonal.c
@@ -0,0 +1,55 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int tk;
+//     scanf("%d",&tk);
+
+//     if(tk>=100)
+//     {
+//         printf("Burger khabo\n");
+//     }
+//     else printf("kissu khabo na\n");
+
+//     return 0;
+// }
+//-----------------------
+// #include <stdio.h>
+// int main()
+// {
+//     int tk;
+//     scanf("%d", &tk);
+
+//     if (tk >= 100)
+//     {
+//         printf("Burger khabo\n");
+//     }
+//     else if (tk >= 50)
+//         printf("Fuchka Khabo\n");
+//     else if (tk >= 20)
+//         printf("ice cream Khabo\n");
+//     else
+//         printf("kissu khabo na\n");
+
+//     return 0;
+// }
+
+#include <stdio.h>//nested if else
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+
+    if (tk >= 5000)
+    {
+        printf("Cox's bazare jabo\n");
+        if (tk >= 10000)
+            printf("Saint martin jabo\n");
+        else
+            printf("Back to home\n");
+    }
+
+    else
+        printf("Kothau jabo na\n");
+
+    return 0;
+}
